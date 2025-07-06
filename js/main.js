9bbdf928040094d994cd7b6caeec19a0b817e649
Macgyber <esteban3261g@gmail.com>
2025-07-06 12:17:58 -0500

    se eliminan elementos que solo hacen el proyecto mas largo y mas sucio

--- js/main.js
+++
@@ -1,8 +0,0 @@
- 
-      window.addEventListener('DOMContentLoaded', function() {
-        var audi = document.getElementById('audi');
-        if (audi) {
-          audi.play().catch(function(){});
-        }
-      });
-    
